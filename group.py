77810b13886cf5426d03783813171b7b6773a380
Caraxng <120607050+Caraxng@users.noreply.github.com>
2023-03-05 03:03:53 +0100

    La

---
+++ group.py
@@ -0,0 +1,43 @@
+#group = ('1 2 3 4 5 5 5 3 3 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20')
+#newl = group.split()
+#for x in newl:
+#    if 3 in x > 1:
+#        print('fizz')
+#    if 5 in x > 1:
+#        print('buzz')
+#        print(x)
+#
+
+#while True:
+#    alln = count(group)
+
+#x = int(fizz)
+#print(x)
+
+# client = input km numbers 
+#if client is > 0 : continue
+#  else
+#      break
+#fueln = client * 100
+#while True:
+#     fueln >= 1500
+#else
+#    print('low fuel')
+#    print('Amount Of Fuel Needed Is:',fueln)
+
+
+
+
+
+#n = 0
+#for number in n:
+#   n = n + 3
+#   nnw = sort n
+#   print(nnw)
+
+
+inn = input('Enter Number:')
+while True:
+   adnum = inn + 1
+   if inn < 0: break 
+   print(adnum)
